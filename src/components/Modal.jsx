19271349b1ef2d11e91abca1112f0309311e2a9a
Vladislav <vladgm233223@gmail.com>
2023-02-18 15:43:33 +0200

    add drag and drop

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,35 +1,80 @@
 import { Box, Typography } from "@mui/material";
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { selectUsers } from "../store/get-users/users-slice";
+import { addColumn } from "../store/selected-columns/selected-columns-slice";
 import { removedColumnUtil } from "../utils/available";
+
 //import { addColumn } from "../store/selected-columns/selected-columns-slice";
 
 export const Modal = () => {
-  //const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  const [currentList, setCurrentList] = useState([]);
+
+  const [currentItem, setCurrentItem] = useState([]);
 
   const users = useSelector(selectUsers);
 
   const selectedColumns = useSelector((state) => state.selectedColumn);
   const [availableColumns] = removedColumnUtil(users[0], selectedColumns);
 
+  const DragStartHandler = (e, card, list) => {
+    setCurrentItem(card);
+    setCurrentList(list);
+  };
+  const dragEndHandler = (e) => {};
+  const dragLeaveHandler = (e) => {};
+
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+  };
+  const dropHandler = (e, list) => {
+    e.preventDefault();
+    if (currentList[0] !== list[0]) {
+      dispatch(addColumn(currentItem));
+    }
+  };
+
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{
+        
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        
+        
+      }}
+    >
       <Box sx={{ mr: "50px" }}>
-        <Typography variant="h6">available</Typography>
+        <Typography variant="h6">Available columns</Typography>
         {availableColumns.map((el) => (
           <Typography
             key={el}
+            onDragStart={(e) => DragStartHandler(e, el, availableColumns)}
+            onDragLeave={(e) => dragLeaveHandler(e)}
+            onDragEnd={(e) => dragEndHandler(e)}
+            onDragOver={(e) => dragOverHandler(e)}
+            onDrop={(e) => dropHandler(e, availableColumns)}
             draggable={true}
-            sx={{ background: "yellow", m: "5px" }}
+            sx={{ m: "5px" }}
           >
             {el}
           </Typography>
         ))}
       </Box>
-      <Box>
-        <Typography variant="h6">selected</Typography>
+      <Box
+        onDragLeave={(e) => dragLeaveHandler(e)}
+        onDragEnd={(e) => dragEndHandler(e)}
+        onDragOver={(e) => dragOverHandler(e)}
+        onDrop={(e) => dropHandler(e, selectedColumns)}
+      >
+        <Typography variant="h6">Selected columns</Typography>
         {selectedColumns.map((el) => (
-          <Typography key={el}>{el}</Typography>
+          <Typography key={el} sx={{ m: "5px" }}>
+            {el}
+          </Typography>
         ))}
       </Box>
     </Box>
