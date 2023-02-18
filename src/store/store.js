27a1ea98d3b59f2c7561e3c4e27b76a018298a6f
Vladislav <vladgm233223@gmail.com>
2023-02-18 13:27:37 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,15 @@ import axios from "axios";
 import * as api from "./config";
 import { usersReduser } from "./get-users/users-slice";
 import { modalReduser } from "./modal-show/modal-slice";
+import { selectedColumnsReducer } from "./selected-columns/selected-columns-slice";
 
 export const store = configureStore({
-  reducer: { isModalOpen: modalReduser, users: usersReduser },
+  reducer: {
+    isModalOpen: modalReduser,
+    users: usersReduser,
+    selectedColumn: selectedColumnsReducer,
+   
+  },
   devTools: true,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
