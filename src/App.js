9c1096c8dfb6da00e350d19d88da95589a2915ab
Vladislav <vladgm233223@gmail.com>
2023-02-21 12:49:48 +0200

    add container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Container } from "@mui/material";
 import { SelectColums } from "./components/SelectColums";
 import { UsersTable } from "./components/UsersTable";
 
+
 function App() {
+  
   return (
     <>
-      <SelectColums />
-      <UsersTable />
+      <Container
+        sx={{ background: "#1B2021", my: "15px", borderRadius: "25px" }}
+      >
+        <SelectColums />
+        <UsersTable />
+      </Container>
+      
     </>
   );
 }
